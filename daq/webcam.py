5585dd01bb3d6a4ee42d48797dc4b49cc5d72d30
Simon Friedmann <simonf256@googlemail.com>
2017-04-21 22:50:06 +0200

    Camera tuning

--- daq/webcam.py
+++ daq/webcam.py
@@ -10,13 +10,17 @@ cam.resolution = (800, 600)
 #cam.framerate = Fraction(1, 6)
 #cam.iso = 800
 cam.annotate_text_size = 12
+cam.vflip = True
+cam.awb_mode = 'auto'
+cam.exposure_mode = 'auto'
+cam.drc_strength = 'high'
 
 time.sleep(2)
 
 while True:
     dt = datetime.datetime.now()
     cam.annotate_text = dt.strftime('%Y-%m-%d  %H:%M')
-    cam.capture('/home/pi/webcam/webcam.jpg')
+    cam.capture('/home/pi/webcam/webcam.jpg', quality=10)
 
     print "Capture at {}".format(dt)
 
